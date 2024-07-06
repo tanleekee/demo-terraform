3e795b638da10e669457535bbc3621a09238ce59
Lee Kee Tan <leekee.tan@gmail.com>
2024-07-06 09:27:49 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "aws_security_group" "web_server_sg" {
   name_prefix = "web-server-sg-"
 
   ingress {
+    from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
