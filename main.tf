137c69a85b500ec0b7be90c996591a7ecc8f87e3
Lee Kee Tan <leekee.tan@gmail.com>
2024-07-06 12:39:59 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-2" # Replace with your desired AWS region
+  region = "us-east-1" # Replace with your desired AWS region
 }
 
 resource "aws_db_instance" "demo_db" {
